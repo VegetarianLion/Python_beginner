270dfcd1bacf14972c6212747bf116b186f5aa86
绘声绘色 <493992079@qq.com>
2016-09-08 18:27:40 +0800

    Create Iterable and Iterator.py

---
+++ Iterable and Iterator.py
@@ -0,0 +1,22 @@
+>>> from collections import Iterable
+>>> isinstance([], Iterable)
+True
+>>> isinstance({}, Iterable)
+True
+>>> isinstance('abc', Iterable)
+True
+>>> isinstance((x for x in range(10)), Iterable)
+True
+>>> isinstance(100, Iterable)
+False
+
+
+>>> from collections import Iterator
+>>> isinstance((x for x in range(10)), Iterator)
+True
+>>> isinstance([], Iterator)
+False
+>>> isinstance({}, Iterator)
+False
+>>> isinstance('abc', Iterator)
+False

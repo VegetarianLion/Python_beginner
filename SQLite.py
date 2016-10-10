a7ffb2bcf4bfa31a040d348de14ae496452552c1
绘声绘色 <493992079@qq.com>
2016-10-10 18:07:52 +0800

    Update SQLite.py

--- SQLite.py
+++ SQLite.py
@@ -1,4 +1,5 @@
-Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
+
+Python 3.4.2 (default, Oct 19 2014, 13:31:11)
 [GCC 4.9.1] on linux
 Type "copyright", "credits" or "license()" for more information.
 >>> import sqlite3
